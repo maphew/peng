2c2cc062a232ddcd5929a0e960fed3a346ab70c2
maphew <maphew@gmail.com>
2025-07-11 18:08:34 -0700

    add acceleration, export stats, started on spike collisions

--- scenes/main-character.gd
+++ scenes/main-character.gd
@@ -5,21 +5,43 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+@onready var spikes: TileMapLayer = $"../spikes"
+
+@export_category("Stats")
+@export var acceleration: float = 5.0
+@export var max_speed: float = 500.0
+@export var min_speed: float = 1.0
+
+
+
+func _ready() -> void:
+	pass
+	#var hazard_area: Area2D = spikes.get_child("Area2D")
+	#hazard_area.connect.body_entered(_on_hazard_collision)
+
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	#old_movement(delta)
+
+		# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+	
+		# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	
+	if Input.is_action_pressed("mv_right"):
+		velocity.x += acceleration
+		velocity.x = clampf(velocity.x, min_speed, max_speed)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("mv_left", "mv_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		print("Current Velocity: ", velocity.x)
+	
+	
 	move_and_slide()
+
+
+
+func _on_hazard_collision() -> void:
+	print("COLLLISSION!!")
